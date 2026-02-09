2860d095f98e351a4755193a5499da696c6d2000
davidtroglin-gif <davidtroglin@gmail.com>
2026-02-09 17:19:05 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,79 +1,4 @@
-// db.js (no modules) - attaches db helpers to window.db
-(function () {
-  function openDB() {
-    return new Promise((resolve, reject) => {
-      const req = indexedDB.open("mtngFormsDB", 1);
-      req.onupgradeneeded = () => {
-        const db = req.result;
-        db.createObjectStore("drafts", { keyPath: "submissionId" });
-        db.createObjectStore("queue", { keyPath: "submissionId" });
-      };
-      req.onsuccess = () => resolve(req.result);
-      req.onerror = () => reject(req.error);
-    });
-  }
-
-  async function put(store, value) {
-    const db = await openDB();
-    return new Promise((resolve, reject) => {
-      const tx = db.transaction(store, "readwrite");
-      tx.objectStore(store).put(value);
-      tx.oncomplete = resolve;
-      tx.onerror = () => reject(tx.error);
-    });
-  }
-
-  async function get(store, key) {
-    const db = await openDB();
-    return new Promise((resolve, reject) => {
-      const tx = db.transaction(store, "readonly");
-      const req = tx.objectStore(store).get(key);
-      req.onsuccess = () => resolve(req.result || null);
-      req.onerror = () => reject(req.error);
-    });
-  }
-
-  async function getAll(store) {
-    const db = await openDB();
-    return new Promise((resolve, reject) => {
-      const tx = db.transaction(store, "readonly");
-      const req = tx.objectStore(store).getAll();
-      req.onsuccess = () => resolve(req.result || []);
-      req.onerror = () => reject(req.error);
-    });
-  }
-
-  async function del(store, key) {
-    const db = await openDB();
-    return new Promise((resolve, reject) => {
-      const tx = db.transaction(store, "readwrite");
-      tx.objectStore(store).delete(key);
-      tx.oncomplete = resolve;
-      tx.onerror = () => reject(tx.error);
-    });
-  }
-
-  window.db = { put, get, getAll, del };
-})();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function openDB() {
+function openDB() {
   return new Promise((resolve, reject) => {
     const req = indexedDB.open("mtngFormsDB", 1);
     req.onupgradeneeded = () => {
@@ -123,5 +48,5 @@ async function del(store, key) {
   });
 }
 
+export const db = { put, get, getAll, del };
 
-export const db = { put, get, getAll, del };*/
